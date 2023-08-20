4f4e8f2328bbeedaf7760b0b6138511629ba9784
Vedant <76871277+theofficialvedantjoshi@users.noreply.github.com>
2023-08-20 21:29:42 +0530

    Update script.py

--- script.py
+++ script.py
@@ -14,13 +14,15 @@ st.set_page_config(
 )
 st.title("🌕🔗YOUTUBE SCRIPT GPT")
 prompt = st.text_input("Add the topic of your video here")
+prompt_time = st.text_input("Add the duration of your video here(Min;sec)")
+prompt_tone= st.text_input("Add the tone of your video")
 title_template = PromptTemplate(
     input_variables = ['topic'],
     template='write me a youtube video title about {topic}'
 )
 script_template = PromptTemplate(
-    input_variables = ['title'],
-    template='write a youtube video script on {title} for a 10 minute video'
+    input_variables = ['title','time','tone'],
+    template='write me a youtube video script based on this title TITLE: {title} so that the length of the video is {time} long and the tone of the script is {tone}'
 )
 
 #memory
@@ -31,13 +33,12 @@ memory = ConversationBufferMemory(input_key='topic',memory_key='chat_history')
 llm = OpenAI(temperature=0.9,max_tokens = 2056)
 title_chain = LLMChain(llm=llm,prompt=title_template,verbose=True,output_key='title',memory=memory)
 script_chain = LLMChain(llm=llm,prompt=script_template,verbose=True,output_key='script',memory=memory)
-sequential_chain = SequentialChain(chains=[title_chain,script_chain],input_variables=['topic'],output_variables=['title','script'],verbose=True)
-if prompt:
-    response = sequential_chain({'topic': prompt})
+sequential_chain = SequentialChain(chains=[title_chain,script_chain],input_variables=['topic','time','tone'],output_variables=['title','script'],verbose=True)
+if prompt and prompt_time and prompt_tone:
+    response = sequential_chain({'topic': prompt,'time': prompt_time,'tone': prompt_tone})
     st.write('Title:')
     st.write(response['title'])
     st.write('Video Script:')
     st.write(response['script'])
     with st.expander('Prompt History'):
        st.info(memory.buffer)
-
